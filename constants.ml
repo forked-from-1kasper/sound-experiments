0f0dc2f3659db28b60e805238e9936f8e88f864e
Siegmentation <siegmentationfault@yandex.ru>
2020-05-30 00:15:38 +0700

    simplify

--- constants.ml
+++ constants.ml
@@ -1,6 +1,6 @@
-let frate    : int ref   = ref 11025
-let elemSecs : float ref = ref 1.81405
-let eps      : float ref = ref 0.00000001
+let frate    = ref 11025
+let elemSecs = ref 1.81405
+let eps      = ref 0.00000001
 
 let noteDecayRate = ref 10.0
 
